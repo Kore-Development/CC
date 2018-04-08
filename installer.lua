0f5d1b08a55fe4d744d181886c3ee99564003182
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2018-04-08 14:22:02 -0500

    Update and rename installer.lua to install.lua

--- installer.lua
+++
@@ -1,6 +0,0 @@
-if not fs.exists("/.temp") then
-  shell.run("mkdir /.temp")
-end
-shell.run("wget https://raw.githubusercontent.com/Kore-Development/CC/master/", programName, " /.temp/install")
-shell.run("/.temp/install")
-fs.delete("/.temp/install")
