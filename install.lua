822cc41c06ba8dd80adacb4de1c5ba9f94d6c9f9
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2018-04-08 14:27:18 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,6 +2,6 @@ args = {...}
 if not fs.exists("/.temp") then
   shell.run("mkdir /.temp")
 end
-shell.run("wget https://raw.githubusercontent.com/Kore-Development/CC/master/", args[1], ".lua", " /.temp/install")
+shell.run("wget https://kore-development.github.io/CC/", args[1], ".lua", " /.temp/install")
 shell.run("/.temp/install")
 fs.delete("/.temp/install")
